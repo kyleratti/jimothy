f35c57238cba16c6666ba5e76682e7794a254530
Kyle <kyle.ratti@outlook.com>
2017-02-27 23:21:45 -0500

    Added auth controller

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,44 @@
+<?php
+namespace App\Http\Controllers;
+
+use Invisnik\LaravelSteamAuth\SteamAuth;
+use App\User;
+use Auth;
+
+class AuthController extends Controller
+{
+    /**
+     * @var SteamAuth
+     */
+    private $steam;
+
+    public function __construct(SteamAuth $steam)
+    {
+        $this->steam = $steam;
+    }
+
+    public function login()
+    {
+        if ($this->steam->validate()) {
+            $info = $this->steam->getUserInfo();
+            if (!is_null($info)) {
+                $user = User::where('steam64', $info->steamID64)->first();
+                if (is_null($user)) {
+                    $user = User::create([
+                        'steam64' => $info->steamID64,
+                        'steam_name' => $info->personaname,
+                        'avatar' => $info->avatarfull
+                    ]);
+                }
+                Auth::login($user, true);
+                return redirect()->route('forum.index'); // redirect to site
+            }
+        }
+        return $this->steam->redirect(); // redirect to Steam login page
+    }
+
+    public function logout() {
+        Auth::logout();
+        return redirect()->route('forum.index');
+    }
+}
