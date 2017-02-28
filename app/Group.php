07ec8fc3bb6f2386bff632fad1ab7e2d20e58046
Kyle <kyle.ratti@outlook.com>
2017-02-27 23:21:37 -0500

    Added Group model

---
+++ app/Group.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Group extends Model
+{
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name', 'inherits_from', 'enabled'
+    ];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+    ];
+}
