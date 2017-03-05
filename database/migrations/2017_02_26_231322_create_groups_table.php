7bb4b70f81724c3501be62ad9cb90c7990005fb8
Kyle <kyle.ratti@outlook.com>
2017-03-05 18:14:07 -0500

    Added roles to groups, removed enabled flag

--- database/migrations/2017_02_26_231322_create_groups_table.php
+++ database/migrations/2017_02_26_231322_create_groups_table.php
@@ -4,6 +4,14 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
+use App\Group;
+
+define('ROLE_GUEST', 0);
+define('ROLE_MODERATOR', 1);
+define('ROLE_ADMIN', 2);
+define('ROLE_SUPERADMIN', 3);
+define('ROLE_OWNER', 4);
+
 class CreateGroupsTable extends Migration
 {
     /**
@@ -16,12 +24,22 @@ class CreateGroupsTable extends Migration
         Schema::create('groups', function (Blueprint $table) {
             $table->increments('id');
             $table->string('name', 16)->unique();
-            $table->string('inherits_from', 16)->nullable();
-            $table->boolean('enabled')->default(true);
+            $table->integer('inherits_from')->nullable();
+            $table->smallInteger('role')->default(0);
             $table->timestamps();
 
-            $table->foreign('inherits_from')->references('name')->on('groups');
+            $table->foreign('inherits_from')->references('id')->on('groups');
         });
+
+        Group::create([
+            'name' => 'Guest',
+            'role_type' => ROLE_GUEST
+        ]);
+
+        Group::create([
+            'name' => 'Owner',
+            'role' => ROLE_OWNER
+        ]);
     }
 
     /**
