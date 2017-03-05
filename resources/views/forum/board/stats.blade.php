98d64596896d28e1cd9da23afaf549977e71f3fe
Kyle <kyle.ratti@outlook.com>
2017-03-05 17:57:07 -0500

    Fixed grammar error

--- resources/views/forum/board/stats.blade.php
+++ resources/views/forum/board/stats.blade.php
@@ -1,3 +1,3 @@
 <div class="board-stats">
-    <b>{{ $iNumMembers }}</b> member{{ $iNumMembers == 1 ? '' : 's' }} have shared <b>{{ $iNumPosts }}</b> post{{ $iNumPosts == 1 ? '' : 's' }} across <b>{{ $iNumThreads }}</b> thread{{ $iNumThreads == 1 ? '' : 's' }}
+    <b>{{ $iNumMembers }}</b> member{{ $iNumMembers == 1 ? ' has' : 's have' }} shared <b>{{ $iNumPosts }}</b> post{{ $iNumPosts == 1 ? '' : 's' }} across <b>{{ $iNumThreads }}</b> thread{{ $iNumThreads == 1 ? '' : 's' }}
 </div>
