bef4b1d385ae45e30470b97d135a80a5daf14bcb
Kyle <kyle.ratti@outlook.com>
2017-03-05 13:02:59 -0500

    Fixed login page not showing anything

--- resources/views/forum/user/login.blade.php
+++ resources/views/forum/user/login.blade.php
@@ -1,5 +1,5 @@
 @extends('layouts.master')
 
-@section('content')
+@section('main')
     <a href="{{ route('forum.user.login.go') }}"><img src="https://steamcommunity-a.akamaihd.net/public/images/signinthroughsteam/sits_02.png" alt="Sign in through Steam"></a>
 @stop
