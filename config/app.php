4781ea0b1bda9b214440f16df14763fdb84f8c9c
Kyle <kyle.ratti@outlook.com>
2017-03-05 16:42:24 -0500

    Made app name read from .env file

--- config/app.php
+++ config/app.php
@@ -12,7 +12,7 @@ return [
     | any other location as required by the application or its packages.
     */
 
-    'name' => 'Aaron\'s Servers',
+    'name' => env('APP_NAME', 'Unknown.'),
 
     'codename' => 'jimothy',
 
