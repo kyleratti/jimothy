b6c73a34bb23a4870af900cde4bebbb1503a4d85
Kyle <kyle.ratti@outlook.com>
2017-03-30 21:06:34 -0400

    Added groups migration

---
+++ database/migrations/2017_03_30_194617_create_groups_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateGroupsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('groups', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name', 12)->unique();
+            $table->integer('inherits_from')->nullable()->index();
+            $table->tinyInteger('weight')->unsigned()->index();
+            $table->boolean('is_default')->nullable()->default(false)->index();
+            $table->timestamps();
+
+            $table->foreign('inherits_from')->references('id')->on('groups');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('groups');
+    }
+}
